8f144608c982830972734586c95fe4a14237525f
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-01 21:53:59 +0530

    tab switching

--- src/components/TabsComp.vue
+++ src/components/TabsComp.vue
@@ -1,42 +1,51 @@
 <template>
     <div>
-      <div class="tabs">
-        <button @click="currentTab = 'DailyComp'">Tab 1</button>
-        <button @click="currentTab = 'OverAllComp'">Tab 2</button>
-      </div>
-      <div class="tab-content">
-        <component :is="currentTab"></component>
-      </div>
+        <div class="tabs">
+            <button :class="{ active: currentTab === 'DailyComp' }" @click="currentTab = 'DailyComp'">Daily</button>
+            <button :class="{ active: currentTab === 'OverallComp' }" @click="currentTab = 'OverallComp'">Overall</button>
+        </div>
+        <div class="tab-content">
+            <component :is="currentTab"></component>
+        </div>
     </div>
-  </template>
-  
-  <script>
-  import DailyComp from './DailyComp.vue';
-  import OverAllComp from './OverAllComp.vue'
-  
-  export default {
+</template>
+
+<script>
+import DailyComp from './DailyComp.vue'
+import OverallComp from './OverAllComp.vue'
+
+export default {
     data() {
-      return {
-        currentTab: 'DailyComp'
-      }
+        return {
+            currentTab: 'DailyComp'
+        }
     },
     components: {
         DailyComp,
-        OverAllComp
+        OverallComp
     }
-  }
-  </script>
-  
-  <style>
-  .tabs {
+}
+</script>
+
+<style>
+.tabs {
     display: flex;
-    justify-content: center;
     gap: 10px;
-    margin: 3rem auto;
-  }
-  
-  .tab-content {
+    margin-top: 2rem;
+    justify-content: center;
+}
+
+button {
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+button.active {
+    background-color: orange;
+    color: white;
+}
+
+.tab-content {
     margin-top: 20px;
-  }
-  </style>
-  
+}
+</style>
