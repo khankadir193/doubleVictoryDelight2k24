ac83d586fd4a788a408ec6e8c3338bf5adea24b5
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-02 20:42:59 +0530

    trophy container design in daily tabs

--- src/components/DailyComp.vue
+++ src/components/DailyComp.vue
@@ -1,5 +1,9 @@
 <template>
         <LeaderBoardDesc v-if="showLeaderBoard" :title="leaderBoardTitle" :description = "leaderDescription" />
+        <div class="trophy-container">
+            <img src="../assets/images/picture_trophy.png" alt="trophy">
+            <p>Come & join the event the first place waiting for you</p>
+        </div>
 </template>
 
 <script>
@@ -21,5 +25,20 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.trophy-container{
+    display: flex;
+    margin: 3rem auto;
+    justify-content: center;
+    width: 25%;
+    flex-direction: column;
 
+    img{
+        width: 100%;
+    }
+
+    p{
+        width: 190%;
+        color: white;
+    }
+}
 </style>
