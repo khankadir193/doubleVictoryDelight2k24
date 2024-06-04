676f157ea1526b74738c70b850197532f1b91d0b
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-04 16:15:18 +0530

    how to play content alignment

--- src/components/HowToPlayComp.vue
+++ src/components/HowToPlayComp.vue
@@ -19,6 +19,15 @@
                 <p>Start Playing Royal Pattern</p>
             </div>
         </div>
+        <div class="line1"></div>
+        <div class="content">
+            <h3>What is the Rebate Card?</h3>
+            <p>Exclusively available for oyeTalk Royal Pattern users.
+                the Rebate Card is a premium in-game item. if you don't 
+                win a game (by not betting on the winning seat). using the
+                Rebate  Card guarantees a 5% bean rebate to reduce your losse.
+            </p>
+        </div>
     </div>
 </template>
 
@@ -33,7 +42,7 @@ export default {
     background-image: url('../assets/images/howToPlayBg.png');
     background-size: 100% 100%;
     width: 90%;
-    height: 40vh;
+    height: 50vh;
     margin: 0 auto;
 }
 
@@ -62,7 +71,7 @@ export default {
 
 .howToPlay-reward {
     display: flex;
-    width: 15%;
+    width: 77%;
     height: 7vh;
     justify-content: center;
     gap: 10vw;
@@ -95,5 +104,27 @@ export default {
             font-size: 10px;
         }
     }
+}
+
+.line1 {
+    width: 93%;
+    height: .02667rem;
+    background: #ff8340;
+    /* opacity: .1; */
+    margin: 0 auto;
+    position: relative;
+    top: 6rem;
+}
+
+.content{
+    display: flex;
+    flex-direction: column;
+    margin: 5.5rem;
+    width: 100%;
+    /* justify-content: flex-start; */
+    /* padding-inline: 4vw; */
+    /* padding-inline-end: 4vw; */
+    position: relative;
+    right: 5rem;
 }
 </style>
